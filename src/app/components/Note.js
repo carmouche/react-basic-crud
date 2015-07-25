0aae097612ab2ffd740d723ac6f5d135820f0601
Selena Carmouche <itscarmouche@gmail.com>
2015-07-24 22:13:05 -0700

    Converted notes app to ES6 syntax

---
+++ src/app/components/Note.js
@@ -0,0 +1,99 @@
+import React from 'react/addons';
+
+/*
+ * @class Note
+ * @extends React.Component
+ */
+class Note extends React.Component {
+
+  // getInitialState () {
+  //   return {editing: false}
+  // }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false
+    };
+
+    this.edit = this.edit.bind(this);
+    this.save = this.save.bind(this);
+    this.remove = this.remove.bind(this);
+    this.renderDisplay = this.renderDisplay.bind(this);
+    this.renderForm = this.renderForm.bind(this);
+
+  }
+
+  componentWillMount () {
+    this.style = {
+      right: this.randomBetween(0, window.innerWidth - 150) + 'px',
+      top: this.randomBetween(0, window.innerHeight - 150) + 'px',
+      transform: 'rotate(' + this.randomBetween(-15, 15) + 'deg)'
+    }
+  }
+  
+  componentDidMount () {
+
+    $(React.findDOMNode(this)).draggable();
+  }
+
+  randomBetween (min, max) {
+      return (min + Math.ceil(Math.random() * max));
+  }
+
+  edit () {
+    this.setState({editing: true});
+  }
+
+  save () {
+    this.props.onChange(this.refs.newText.getDOMNode().value, this.props.index);
+    this.setState({editing: false});
+  }
+
+  remove () {
+    this.props.onRemove(this.props.index);
+  }
+
+  renderDisplay () {
+    return (
+      <div className="note"
+          style={this.style}>
+          <p>{this.props.children}</p>
+          <span>
+              <button onClick={this.edit}
+                      className="btn btn-primary glyphicon glyphicon-pencil"/>
+              <button onClick={this.remove}
+                      className="btn btn-danger glyphicon glyphicon-trash"/>
+          </span>
+      </div>
+    );
+  }
+
+  renderForm () {
+    return (
+      <div className="note" style={this.style}>
+      <textarea ref="newText" defaultValue={this.props.children} 
+      className="form-control"></textarea>
+      <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
+      </div>
+    );
+  }
+
+  render () {
+    if (this.state.editing) {
+          return this.renderForm();
+      }
+      else {
+          return this.renderDisplay();
+      }
+  }
+
+}
+
+// Prop types validation
+// Item.propTypes = {
+//   item: React.PropTypes.object.isRequired,
+// };
+
+export default Note;
+
